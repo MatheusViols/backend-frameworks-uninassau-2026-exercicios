6f1117032a25599e994bb08b37ddd17cc77ee30a
Matheus <matheusviolss@gmail.com>
2026-03-02 13:38:14 -0300

    feat: Adição dos metodos update e destroy
    Criação dos handlers put e delete
    Criação dos dos metodos update e destroy
    
    Código passa em todos os testes, mas ainda não está na melhor forma e precisa ser organizado.

--- exercicio-02/src/controllers/ProductController.js
+++ exercicio-02/src/controllers/ProductController.js
@@ -27,32 +27,108 @@
 let products = [];
 let nextId = 1;
 
-function index(req, res) {
-	res.status(200).json(products);
-}
-function store(req, res) {
-	const body = req.body;
+class ProductController {
+
+    dadosValidos(body) {
 	const bodyCompleto = 
-		("name"  in body && body.name  != undefined) &&
-		("price" in body && body.price != undefined) &&
-		("stock" in body && body.stock != undefined);
+	      (body.name  != undefined) &&
+	      (body.price != undefined) &&
+	      (body.stock != undefined) &&
+	      (Number.isInteger(body.price)) &&
+	      (Number.isInteger(body.stock));
+	
 
-	if(bodyCompleto) {
-		const produto = {
-			id: nextId++,
-			name: body.name,
-			price: body.price,
-			stock: body.stock,
-		};
+	
+	return bodyCompleto;
+    }
+    
+    index(req, res) {
+	res.status(200).json(products);
+    }
 
-		products.push(produto);
-		res.status(201).json(produto);
+    show(req, res) {
+	const parametro = parseInt(req.params.id);
+
+	const produtoIndex = products.findIndex((produto) => produto.id === parametro);
+	
+	if(produtoIndex != -1) {
+	    res.status(200).json(products[produtoIndex])
 	} else {
-		res.status(400).json({error: 'Dados inválidos'});
+	    res.status(404).json({
+		error: 'Produto não encontrado',
+		body: {id: parametro},
+	    });
 	}
-}
+    }
 
-module.exports = {
-	index,
-	store
-};
+    store(req, res) {
+	const body = req.body;
+
+	if(this.dadosValidos(body)) {
+	    const produto = {
+		id: nextId++,
+		name: body.name,
+		price: body.price,
+		stock: body.stock,
+	    };
+
+	    products.push(produto);
+	    res.status(201).json(produto);
+	} else {
+	    res.status(400).json({
+		error: 'Dados inválidos',
+		body: body,
+	    });
+	}
+    }
+
+    update(req, res) {
+	const parametro = parseInt(req.params.id);
+	const body = req.body;
+
+	const produtoIndex = products.findIndex((produto) => produto.id === parametro);
+
+	if(produtoIndex != -1) {
+	    if(this.dadosValidos(body)) {
+		products[produtoIndex].name = body.name;
+		products[produtoIndex].price = body.price;
+		products[produtoIndex].stock = body.stock;
+
+		res.status(200).json(products[produtoIndex]);
+	    } else {
+		res.status(400).json({
+		    error: "Dados inválidos",
+		    body: body,
+		});
+	    }
+	} else {
+	    res.status(404).json({
+		error: "Produto não encontrado",
+		body: {id: parametro},
+	    });
+	}
+	
+    }
+
+    destroy(req, res) {
+	const parametro = parseInt(req.params.id);
+
+	const produtoIndex = products.findIndex((produto) => produto.id === parametro);
+
+	if(produtoIndex != -1) {
+	    const quantidadeRemovida = 1;
+	    products.splice(produtoIndex, quantidadeRemovida);
+	    res.status(200).json({
+		body: 'Produto removido com sucesso',
+	    });
+	} else {
+	    res.status(404).json({
+		error: "Produto não encontrado",
+		body: {id: parametro},
+	    });
+	}
+    }
+    
+}
+module.exports = ProductController;
+
--- exercicio-02/src/server.js
+++ exercicio-02/src/server.js
@@ -8,25 +8,46 @@
 // /products/:id -> controller.destroy 6. Iniciar servidor na porta 3000 (se
 // executado diretamente) 7. Exportar app
 
-const express = require('express'); 
-const controller = require('./controllers/ProductController');
+const express = require('express');
+const productController = require('./controllers/ProductController');
 
-const app = express(); 
+
+const app = express();
+const controller = new productController();
+
 const PORTA = 3000;
 
 app.use(express.json());
 
 app.get('/products', (req, res) => { 
-	controller.index(req, res); 
+    controller.index(req, res); 
+});
+
+app.get('/products/:id', (req, res) => {
+    controller.show(req, res);
 });
 
 app.post('/products', (req, res) => {
-	controller.store(req, res);
-});
-/*
-app.listen(PORTA, () => {
-	console.log(`Servidor rodando na porta ${PORTA}`);
+    controller.store(req, res);
 });
 
+app.put('/products/:id', (req, res) => {
+    controller.update(req, res);
+});
+
+app.delete('/products/:id', (req, res) => {
+    controller.destroy(req, res);
+});
+/*
+
+app.listen(PORTA, () => {
+    console.log(`Servidor rodando na porta ${PORTA}`);
+    });
 */
+
+
 module.exports = app;
+
+
+
+
